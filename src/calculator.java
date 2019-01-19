3cfa092489c479aa63bbbe897a00389f01477253
zemiremansur <46762037+zemiremansur@users.noreply.github.com>
2019-01-19 13:04:47 -0500

    i added by my self

--- src/calculator.java
+++ src/calculator.java
@@ -3,16 +3,23 @@ public class calculator {
     public static double add(double  num1,double num2) {
 
        return num1 + num2 ;
-
-public static double muliply(double num1,double num2){
+    }
+    
+    
+   public static double muliply(double num1,double num2){
    return num1*num2;
 
-}
+   }
         
-        
+  
+   public static double Subtract(double  num1,double num2){
+        double result = num1 -num2;
+        return num1-num2;
+    }
+      
         
     }
 
 
 
-}
+
